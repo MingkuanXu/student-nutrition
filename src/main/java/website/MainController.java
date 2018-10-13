9c736f453d5f166d1ae19004c4f8b9b2bedb547b
MingkuanXu <mingkuan.xu@duke.edu>
2018-10-13 18:35:58 -0400

    Set up jdbcTemplate & MySQL

--- src/main/java/website/MainController.java
+++ src/main/java/website/MainController.java
@@ -1,18 +1,32 @@
 package website;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
-
+    
+    
+    @RequestMapping("/comment")
+    public String comment(@RequestParam(name="comment") String comment) {
+        String query = "insert into comments(date,content) values(CURRENT_TIMESTAMP,?)";
+        jdbcTemplate.update(query, comment);
+		return "redirect:/";
+    }
+    
 }
