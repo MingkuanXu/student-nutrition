64cebe2a05379c02c4870a94386d80154e890941
Malcolm Speed <speedylol@Malcolms-MacBook.local>
2018-10-13 23:38:31 -0400

    working Twitter API

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
 import tweepy
+import wget
 
-consumer_key = 'sJ54pjXvx5pc5SXTM6wAabgl7'
-consumer_secret = 'AqHSZ1YD9DXIm3vaPeF95DMRMhcmn93bqhaKdOWtqFIdvoMhKc'
+consumer_key = "IcGlRhFdmCBNYrrxJe42rG8X0"
+consumer_secret = "mLK6PAIgRaAlhfnWMjCAH1HyQD5vQMgXtREAukSNThXxCuwpFq"
 access_token = '1388376492-6rr3u1KbSjvoKTbQWWkmTyynqBfw7L6GnREQer5'
-token_secret = '55DpiudGed4FQ6iYCT453MynH2x7ezfkj2lgfikmLr3QD '
+token_secret = '55DpiudGed4FQ6iYCT453MynH2x7ezfkj2lgfikmLr3QD'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret )
-
-# Redirect user to Twitter to authorize
-redirect_user(auth.get_authorization_url())
-
-# Get access token
-auth.get_access_token("verifier_value")
-
-# Construct the API instance
-api = tweepy.API(auth)
+auth.set_access_token(access_token, token_secret)
 
 """ token = session.get('request_token')
 session.delete('request_token')
@@ -29,4 +22,39 @@ except tweepy.TweepError:
     print 'Error! Failed to get access token.' """
 
 
-api.update_status('hacking')
+api = tweepy.API(auth)
+
+class MyStreamListener(tweepy.StreamListener):
+
+    def onStatus(self, status):
+        return status.text
+
+
+tweets = api.user_timeline(screen_name='stutrition',
+    count=5, include_rts=False,
+    exclude_replies=False)
+last_id = tweets[-1].id
+
+ 
+while (True):
+    more_tweets = api.user_timeline(screen_name='stutrition',
+    count=5,
+    include_rts=False,
+    exclude_replies=False,
+    max_id=last_id-1)
+
+    # There are no more tweets
+    if (len(more_tweets) == 0):
+        break
+    else:
+        last_id = more_tweets[-1].id-1
+        tweets = tweets + more_tweets
+
+media_files = set()
+for status in tweets:
+    media = status.entities.get('media', [])
+    if(len(media) > 0):
+        media_files.add(media[0]['media_url'])
+
+for media_file in media_files:
+    wget.download(media_file)
